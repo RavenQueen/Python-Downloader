fa36005d59c8546eac95299a38d160853daf8c26
RavenQueen <kwrite09@yahoo.com>
2015-02-26 20:00:56 -0800

    Code overhaul
    Add check to downloader to see if we already have the installation file
    and that
    the hash is correct.
    
    Made sure Python is installed before we download the installer to
    uninstall it again. (Do we don't waste time or bandwidth.)
    
    Added changeInstallDir()
    Added a check to see if we already have the installer to
    downloadPython()
    Added a 2 second pause to let the user see what directory Python is set
    to be installed in.
    Made the menu loop until the program is quit.

--- PyDown.py
+++ PyDown.py
@@ -3,9 +3,11 @@ import getpass
 import urllib2
 import hashlib
 import os
+import time
 
 curOS = platform.system()
 installDir = ""
+
 
 def detectOS():
     if curOS == "Windows":
@@ -16,51 +18,62 @@ def detectOS():
     else:
         print "Error: Unknown operating system detected."
 	return curOS
-	
+
+
 def downloadPython():
-	url = "https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi"
-	filename = url.split('/')[-1]
-	u = urllib2.urlopen(url)
-	f = open(filename, 'wb')
-	meta = u.info()
-	file_size = int(meta.getheaders("Content-Length")[0])
-	print "Downloading: %s Bytes: %s" % (filename, file_size)
-	
-	file_size_dl = 0
-	block_sz = 8192
-	while True:
-		buffer = u.read(block_sz)
-		if not buffer:
-			break
+	if fileCheck() == True:
+		hashCheck()
+	else:
+		url = "https://www.python.org/ftp/python/2.7.9/python-2.7.9.msi"
+		filename = url.split('/')[-1]
+		u = urllib2.urlopen(url)
+		f = open(filename, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		print "Downloading: %s Bytes: %s" % (filename, file_size)
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+			buffer = u.read(block_sz)
+			if not buffer:
+				break
 			
-		file_size_dl += len(buffer)
-		f.write(buffer)
-		status = r"%10d [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-		status = status + chr(8)*(len(status)+1)
-		print status,
-	f.close()
-	hashCheck()
-	
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status,
+		f.close()
+		hashCheck()
+
+
 def fileCheck():
 	if os.path.isfile("python-2.7.9.msi"):
 		return True
-	#else
-		#return False
-	
+	else:
+		return False
+
+
 def install():
 	installCmd = "msiexec /i python-2.7.9.msi TARGETDIR=" + installDir
 	os.system(installCmd)
-	
+
+
 def uninstall():
-	if fileCheck() == False:
-		ans = raw_input("Error: The original installer is needed to continue the uninstallation.\nDownload now? Y\N")
-		if ans in ("y", "Y"):
-			print "TODO: Change download section to contain a switch to come back here."
+	if os.path.isfile("C:\\Python27\python.exe"):
+		if fileCheck() == False:
+			ans = raw_input("Error: The original installer is needed to continue the uninstallation.\nDownload now? Y\N: ")
+			if ans in ("y", "Y"):
+				downloadPython()
+				uninstallCmd = "msiexec /x python-2.7.9.msi"
+				os.system(uninstallCmd)
+			else:
+				exit()
 		else:
-			exit()
+			uninstallCmd = "msiexec /x python-2.7.9.msi"
+			os.system(uninstallCmd)
 	else:
-		uninstallCmd = "msiexec /x python-2.7.9.msi"
-		os.system(uninstallCmd)
+		print "Python does not seem to be installed."
 
 def hashCheck():
 	"""Make Sure we have the correct hash and the download has
@@ -73,35 +86,46 @@ def hashCheck():
 		md5_returned = hashlib.md5(data).hexdigest()
 
 	if original_md5 == md5_returned:
-		print "MD5 verified. Beginning installation process."
+		print "MD5 verified. Starting installer."
 	else:
 		print "MD5 verification failed!."
-	install()
-		
+	#install() No longer calls this function here.
+
+
+def changeInstallDir():
+	ans = raw_input("Where would you like to install Python?: ")
+	if ans != "":
+		global installDir
+		installDir = ans
+	print "The installation directory is currently set to: " + installDir
+	time.sleep(2) # Pause 2 seconds so the user can read the directory.
+
 def menu():
-	if curOS == "Linux":
-		os.system('clear')
-	elif curOS == "Windows":
-		os.system('cls')
+	var = 1
+	while var == 1:
+		print "Python Downloader Version 1.0a\n\n"
+		if curOS == "Linux":
+			os.system('clear')
+		elif curOS == "Windows":
+			os.system('cls')
 
-	print "Python Downloader Version 1.0a\n\n"
-
-	print("""
-	1. Install Python
-	2. Uninstall Python
-	3. Change installation directory
-	4. Quit
-	""")
-	ans = raw_input("Enter a selection: ")
-	if ans == "1":
-		print "Downloading python 2.7 installer...\n"
-		downloadPython()
-	elif ans == "2":
-		uninstall()
-	elif ans == "3":
-		print "You have chosen 3!"
-	elif ans == "4":
-		exit()
+		print("""
+		1. Install Python
+		2. Uninstall Python
+		3. Change installation directory
+		4. Quit
+		""")
+		ans = raw_input("Enter a selection: ")
+		if ans == "1":
+			print "Downloading python 2.7 installer...\n"
+			downloadPython() # This downloads the file and calls hashCheck()
+			install()	# Install Python.
+		elif ans == "2":
+			uninstall()
+		elif ans == "3":
+			changeInstallDir()
+		elif ans == "4":
+			exit()
 
 
 detectOS()
